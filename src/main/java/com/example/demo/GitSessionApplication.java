70c5865d4083e1c380e8121cd7c5d237e65e1efd
sidd18021989 <66636905+sidd18021989@users.noreply.github.com>
2023-09-26 09:39:57 +0530

    this is my first commit

---
+++ src/main/java/com/example/demo/GitSessionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSessionApplication.class, args);
+	}
+
+}
